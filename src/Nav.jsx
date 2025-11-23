247a03fe4b34771bec7541b443a960c3b7f139b9
forresttindall <forrest.tindall@gmail.com>
2025-11-23 14:09:06 -0700

    initial commit

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -36,10 +36,10 @@ export default function Nav() {
       </div>
       <div id="mobile-menu" className={`mobile-menu ${menuOpen ? 'open' : ''}`}>
         <div className="mobile-menu-container">
-          <a href="/biography" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Biography</span></a>
-          <a href="/graphicdesign" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Graphic Design</span></a>
           <a href="/websites" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Websites</span></a>
+          <a href="/graphicdesign" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Graphic Design</span></a>
           <a href="/photography" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Photography</span></a>
+          <a href="/biography" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Biography</span></a>
           <a href="/contact" className="nav-link" onClick={() => setMenuOpen(false)}><span className="nav-link-text">Contact</span></a>
           <a href="https://www.instagram.com/forrest.tindall" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="nav-link" onClick={() => setMenuOpen(false)}><InstagramLogo size={18} /></a>
         </div>
